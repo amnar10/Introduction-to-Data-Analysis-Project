aea701f87d3ae20f825a2d3713f1b0d349532c58
amnar10 <amnarajput86@gmail.com>
2021-04-17 15:52:41 +0100

    import of 2 CSV file from the internet

---
+++ Part 1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+vaccinations_country=pd.read_csv("country_vaccinations.csv")
+print(vaccinations_country.head(), vaccinations_country.shape)
+
+import pandas as pd
+
+vaccinations_manufacturer=pd.read_csv("country_vaccinations_by_manufacturer.csv")
+print(vaccinations_manufacturer.head(), vaccinations_manufacturer.shape)
