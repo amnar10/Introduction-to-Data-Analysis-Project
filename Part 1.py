8ad436eaaf2729d8ee286ee2da6bc9811036b8f4
amnar10 <amnarajput86@gmail.com>
2021-04-17 19:39:39 +0100

    missing vlues in vaccinations_country

--- Part 1.py
+++ Part 1.py
@@ -7,3 +7,11 @@ import pandas as pd
 
 vaccinations_manufacturer=pd.read_csv("country_vaccinations_by_manufacturer.csv")
 print(vaccinations_manufacturer.head(), vaccinations_manufacturer.shape)
+
+import pandas as pd
+
+vaccinations_country=pd.read_csv("country_vaccinations.csv")
+print(vaccinations_country.head(), vaccinations_country.shape)
+vaccinations_country
+missing_val=vaccinations_country.isnull()
+print(missing_val)
