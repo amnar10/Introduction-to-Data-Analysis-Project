cc51a854e5172a4cd0286a44202748d8261f8c5e
amnar10 <amnarajput86@gmail.com>
2021-04-18 14:51:19 +0100

    merged data

--- Part 1.py
+++ Part 1.py
@@ -1,30 +1,37 @@
 import pandas as pd
+pd.set_option('display.max_rows', 50)
+pd.set_option('display.max_columns', 20)
+pd.set_option('display.width', 1000)
 
-vaccinations_country=pd.read_csv("country_vaccinations.csv")
+vaccinations_country = pd.read_csv("country_vaccinations.csv")
 print(vaccinations_country.head(), vaccinations_country.shape)
 
 import pandas as pd
 
-vaccinations_manufacturer=pd.read_csv("country_vaccinations_by_manufacturer.csv")
+vaccinations_manufacturer = pd.read_csv("country_vaccinations_by_manufacturer.csv")
 print(vaccinations_manufacturer.head(), vaccinations_manufacturer.shape)
 
 import pandas as pd
 
-vaccinations_country=pd.read_csv("country_vaccinations.csv")
+vaccinations_country = pd.read_csv("country_vaccinations.csv")
 print(vaccinations_country.head(), vaccinations_country.shape)
 vaccinations_country
-missing_val=vaccinations_country.isnull()
+missing_val = vaccinations_country.isnull()
 print(missing_val)
 
-missing_val=vaccinations_country.isnull().sum()
+missing_val = vaccinations_country.isnull().sum()
 print(missing_val)
 
-missing_val=vaccinations_country.fillna(0)
+missing_val = vaccinations_country.fillna(0)
 print(missing_val)
 
-vaccinations_manufacturer=pd.read_csv("country_vaccinations_by_manufacturer.csv")
+vaccinations_manufacturer = pd.read_csv("country_vaccinations_by_manufacturer.csv")
 print(vaccinations_manufacturer.head(), vaccinations_manufacturer.shape)
 
-dropping_duplicates=vaccinations_country.drop_duplicates()
+dropping_duplicates = vaccinations_country.drop_duplicates()
 print(dropping_duplicates.shape)
 
+merged_data = pd.merge(vaccinations_country,vaccinations_manufacturer,on='date')
+print(vaccinations_country.shape, vaccinations_manufacturer.shape)
+print(merged_data.shape)
+print(merged_data)c
