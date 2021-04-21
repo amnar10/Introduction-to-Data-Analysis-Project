b773d48c95237ede0f6c9beb909e44d68abec45e
amnar10 <amnarajput86@gmail.com>
2021-04-21 14:06:34 +0100

    analysing data

--- Part 2.py
+++ Part 2.py
@@ -11,8 +11,27 @@ print(android_games.shape)
 print(android_games.columns)
 print(android_games.head(5))
 
-list=['rank', 'title', 'installs', 'paid']
+list=['rank', 'title', 'installs', 'paid', 'category']
+list1=['title','growth (30 days)','growth (60 days)',]
+list2=['title', 'total ratings', 'average rating', 'paid']
+
 print(android_games[list].head())
+print(android_games[list1].head())
+print(android_games[list2].head())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
