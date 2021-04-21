a503309341906fea6621952b50aa94747cbd80b0
amnar10 <amnarajput86@gmail.com>
2021-04-22 00:53:23 +0100

    analysing data

--- Part 2.py
+++ Part 2.py
@@ -29,12 +29,37 @@ print(np_30_60)
 
 list_total_ratings=['total ratings']
 list_5star_ratings=['5 star ratings']
+list_4star_ratings=['4 star ratings']
+list_3star_ratings=['3 star ratings']
+list_2star_ratings=['2 star ratings']
+list_1star_ratings=['1 star ratings']
 
 total_ratings_np= np.array(android_games[list_total_ratings])
 five_star_ratings_np=np.array(android_games[list_5star_ratings])
+four_star_ratings_np=np.array(android_games[list_4star_ratings])
+three_star_ratings_np=np.array(android_games[list_3star_ratings])
+two_star_ratings_np=np.array(android_games[list_2star_ratings])
+one_star_ratings_np=np.array(android_games[list_1star_ratings])
+
 
 percentage_5_star = five_star_ratings_np / total_ratings_np * 100
 print(percentage_5_star)
+
+percentage_4_star = four_star_ratings_np / total_ratings_np * 100
+print(percentage_4_star)
+
+percentage_3_star = three_star_ratings_np / total_ratings_np * 100
+print(percentage_3_star)
+
+percentage_2_star = two_star_ratings_np / total_ratings_np * 100
+print(percentage_2_star)
+
+percentage_1_star = one_star_ratings_np / total_ratings_np * 100
+print(percentage_1_star)
+
+
+
+
 
 
 
